3bcfa8d570d92bf62b91dc07dbe5391ac1468f64
Ammar Elmesaly <amarmazn4@gmail.com>
2025-07-27 17:50:23 +0300

    Add timestamp functionality

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
+require('dotenv').config();
+let express = require('express');
+let app = express();
+let cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  const date = req.params.date;
+
+  if (formatDate.hasError(date)) {
+    res.send({error: "Invalid Date"});
+  } else {
+    const dateUtc = formatDate.toUtc(date);
+    const dateUnix = formatDate.toUnix(date);
+    res.send({unix: dateUnix, utc: dateUtc});
+  }
+});
 
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+
+let listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+const formatDate = {
+  toUtc(date) {
+    if (this.isUnix(date)) return new Date(Number(date)).toUTCString();
+    if (!date) return new Date().toUTCString();
+    return new Date(date).toUTCString();
+  },
+  toUnix(date) {
+    if (this.isUnix(date)) return Number(date);
+    if (!date) return new Date().getTime();
+    return new Date(date).getTime();
+  },
+  isUnix(date) {
+    return Number.isInteger(Number(date));
+  },
+  hasError(date) {
+    return isNaN(new Date(date)) && !this.isUnix(date);
+  }
+}
